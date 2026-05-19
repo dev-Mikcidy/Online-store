5012b5c04de4f9200ee09568d01bc3f98b847b56
Michael2024-coder <Agunbiademichael2017@gmail.com>
2026-05-19 10:38:55 +0200

    Add adminRoute to indexRoutes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import productRoute from "./productRoute.js";
 import authRoute from "./authRoute.js";
 import paymentRoute from "./paymentRoute.js"
+import adminRoute from "./adminRoute.js"
 
 export const router = express.Router();
 
 router.use("/api", productRoute);
 router.use("/api/auth", authRoute);
 router.use("/api", paymentRoute)
+router.use("/api/admin", adminRoute)
