f7030f01fb0314504d4d5abb8a8bf590e956b29a
Maraizu Dominic-Judes <mars@Maraizus-MacBook-Air.local>
2026-05-25 00:11:58 +0200

    Debug the payment model

--- backend/src/config/stripe.js
+++ backend/src/config/stripe.js
@@ -5,5 +5,7 @@ dotenv.config();
 const stripe = {
   apiKey: process.env.STRIPE_API_KEY,
   endpointSecret: process.env.ENDPOINT_SECRET_KEY,
+  successURL: process.env.SUCCESS_URL,
+  cancelURL: process.env.CANCEL_URL
 };
 export default stripe;
